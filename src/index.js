78a0fdb46362006013facb076887b3135e571482
DimaBog22 <bogdanov111222@yandex.ru>
2018-10-03 15:13:35 +0300

    Create function Advanced Zeros 2.0

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
-module.exports = function getZerosCount(number, base) {
-    let zeros = 0;
-    let n = +parseInt(number,base);
-
-    while (n > 0) {
-        n = Math.floor(n / 5);
-        zeros = zeros + n;
+module.exports = function getZerosCount(number, base) { 
+    const numbers = {}; 
+    const results = []; 
+    const multipliers = getMultipliers(base); 
+    
+    Object.keys(multipliers).forEach(multiplier => { 
+    const count = getSpecificNumbersCount(number, multiplier); 
+    const result = Math.floor(count / multipliers[multiplier]); 
+    results.push(result); 
+    }); 
+    
+    return Math.min.apply(null, results); 
     } 
-    return zeros;
-}
+    
+    function getMultipliers(base) { 
+    let numbers = {}; 
+    let del = 2; 
+    
+    while (base != 1) { 
+    while (base % del != 0) { 
+    del++; 
+    } 
+    base = base / del; 
+    if (numbers[del]) { 
+    numbers[del]++; 
+    } else { 
+    numbers[del] = 1; 
+    } 
+    } 
+    return numbers; 
+    } 
+    
+    
+    function getSpecificNumbersCount(number, divider) { 
+    let zeros = 0; 
+    
+    while (number > 0) { 
+    number = Math.floor(number / divider); 
+    zeros = zeros + number; 
+    } 
+    return zeros; 
+    }
